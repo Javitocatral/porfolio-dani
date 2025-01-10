6b90fc2109310186622a0eaa6f4ea4e88e3ee154
Javier Gascon <javitocatral@gmail.com>
2025-01-10 11:04:18 +0100

    gradiante

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import dani from './assets/dani.png'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gradient, setGradient] = useState(
+    'linear-gradient(to right, #add8e6, #d3d3d3)'
+  )
 
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event
+    const width = window.innerWidth
+    const height = window.innerHeight
+
+    // Calcular la nueva posición del gradiente basado en la posición del ratón
+    const xPercent = (clientX / width) * 100
+    const yPercent = (clientY / height) * 100
+
+    // Modificar el gradiente basado en la posición del ratón
+    const newGradient = `radial-gradient(circle at ${xPercent}% ${yPercent}%,rgb(132, 189, 207),rgb(240, 240, 240))`
+    setGradient(newGradient)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="container"
+        onMouseMove={handleMouseMove}
+        style={{ background: gradient }}
+      >
+        <div>
+          <img
+            style={{ width: '500px' }}
+            src={dani}
+            alt="foto de medio cuerpo de daniel"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
