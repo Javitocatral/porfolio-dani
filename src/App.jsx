1703cc709513c3af0e68d07c935e0dd2d4c6eff0
Javier Gascon <javitocatral@gmail.com>
2025-01-13 10:47:03 +0100

    sobre mi

--- src/App.jsx
+++ src/App.jsx
@@ -35,25 +35,27 @@ function App() {
         <div>
           <h2>Sobre mí:</h2>
           <p>
-            {' '}
             Soy animador de personajes, gran apasionado de la animación y con
             muy buena actitud. Siempre estando al día en mi formación, lo último
             que he realizado ha sido un curso en Animworkshop, dónde realicé
             planos de animación 3d enfocados en la fiscalidad de los personajes
-            y profundicé aún más mis conocimientos de Autodesk Maya. Me encanta
-            la animación y el dibujo por su característica de transmitir
-            historias. Tengo una sólida base en materia audiovisual, ilustración
-            y principios de la animación. Antes de comenzar en la animación hice
-            mis trabajos como ilustrador y diseño de personajes en un videojuego
-            “Wardians” Después de realizar el máster en la U-tad de animación,
-            trabajé en un largometraje “Buffalo Kids” y un corto “Bright” que
-            salió ganador como el más votado de la Company Day. Mi aptitud me
-            ayudó a integrarme perfectamente en el flujo de trabajo de los
-            estudios y esta experiencia me ha servido para consolidarme en mi
-            trabajo. Estoy entusiasmado por seguir creciendo profesionalmente en
+            y profundicé aún más mis conocimientos de Autodesk Maya.
+            <br />
+            Me encanta la animación y el dibujo por su característica de
+            transmitir historias. Tengo una sólida base en materia audiovisual,
+            ilustración y principios de la animación.
+            <br /> Antes de comenzar en la animación hice mis trabajos como
+            ilustrador y diseño de personajes en un videojuego “Wardians”.
+            <br />
+            Después de realizar el máster en la U-tad de animación, trabajé en
+            un largometraje “Buffalo Kids” y un corto “Bright” que salió ganador
+            como el más votado de la Company Day. Mi aptitud me ayudó a
+            integrarme perfectamente en el flujo de trabajo de los estudios y
+            esta experiencia me ha servido para consolidarme en mi trabajo.
+            <br /> Estoy entusiasmado por seguir creciendo profesionalmente en
             este apartado y aportar mi creatividad y energía al equipo y se
             transmita al resultado final de la producción. Si te interesa
-            conocer más estaré encantado de conectar contigo{' '}
+            conocer más estaré encantado de conectar contigo.
           </p>
         </div>
       </div>
